899fa9f1699b4c81c40973d199f0526c98c9602c
Noah Gerberick <nwg7199@gmail.com>
2026-04-10 10:33:42 -0700

    Add S3 encryption, website, and policy support

--- modules/aws/storage/s3/main.tf
+++ modules/aws/storage/s3/main.tf
@@ -24,6 +24,20 @@ resource "aws_s3_bucket_versioning" "this" {
   }
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  count = var.encryption_enabled ? 1 : 0
+
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    bucket_key_enabled = var.bucket_key_enabled
+
+    apply_server_side_encryption_by_default {
+      sse_algorithm = var.sse_algorithm
+    }
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "this" {
   count = var.block_public_access ? 1 : 0
 
@@ -78,4 +92,21 @@ resource "aws_s3_bucket_logging" "this" {
 
   target_bucket = var.logging_target_bucket
   target_prefix = var.logging_target_prefix
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  count = var.bucket_policy_json != null ? 1 : 0
+
+  bucket = aws_s3_bucket.this.id
+  policy = var.bucket_policy_json
+}
+
+resource "aws_s3_bucket_website_configuration" "this" {
+  count = var.website_enabled ? 1 : 0
+
+  bucket = aws_s3_bucket.this.id
+
+  index_document {
+    suffix = var.website_index_document
+  }
 }
